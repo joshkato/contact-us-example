2a9b31d2b05898985567f27b430dde41b25a18e6
Josh K <joshkato@users.noreply.github.com>
2021-01-12 21:29:40 -0500

    Implement some rudimentary validation logic for a contact message.

--- src/ContactUsExample/Services/ContactUsService.cs
+++ src/ContactUsExample/Services/ContactUsService.cs
@@ -1,3 +1,4 @@
+using System.Net.Mail;
 using System.Threading.Tasks;
 using ContactUsExample.Models;
 using Microsoft.Extensions.Logging;
@@ -20,7 +21,35 @@ namespace ContactUsExample.Services
 
         public async ValueTask<IServiceError> SubmitMessageAsync(ContactUsMessage message)
         {
+            Log.LogDebug("Validating incoming message...");
+            var validationError = ValidateMessage(message);
+            if (validationError != null)
+                return validationError;
+
+            Log.LogDebug("Saving message...");
+            // TODO: Implement actually saving the message.
+
             await Task.CompletedTask.ConfigureAwait(false);
+            return null;
+        }
+
+        private IServiceError ValidateMessage(ContactUsMessage message)
+        {
+            if (string.IsNullOrWhiteSpace(message.FirstName))
+                return new ServiceError($"Required field \"{nameof(ContactUsMessage.FirstName)}\" has not been populated.");
+
+            if (string.IsNullOrWhiteSpace(message.LastName))
+                return new ServiceError($"Required field \"{nameof(ContactUsMessage.LastName)}\" has not been populated.");
+
+            if (string.IsNullOrWhiteSpace(message.Message))
+                return new ServiceError($"Required field \"{nameof(ContactUsMessage.Message)}\" has not been populated.");
+
+            if (string.IsNullOrWhiteSpace(message.Email))
+                return new ServiceError($"Required field \"{nameof(ContactUsMessage.Email)}\" has not been populated.");
+
+            if (!MailAddress.TryCreate(message.Email, out _))
+                return new ServiceError($"Provided email address {message.Email} is not a valid email address.");
+
             return null;
         }
     }
